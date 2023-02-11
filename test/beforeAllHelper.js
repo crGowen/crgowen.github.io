f1e199ef3e700a346f4a76553bdcdcc9d56a1029
crgowen <private_email>
2023-02-11 21:37:58 +0000

    debug 3

--- test/beforeAllHelper.js
+++ test/beforeAllHelper.js
@@ -2,7 +2,7 @@ const server = require("../scripts/server.js");
 const process = require("process");
 
 module.exports = () => {
-    const useGithubPagesSite = process.env.USE_GH_PAGES_SITE === "enable";
+    const useGithubPagesSite = process.env.USE_GH_PAGES_SITE;
 
     let app = null;
     let url = "https://crgowen.github.io";
