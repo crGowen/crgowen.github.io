cc75e877ab5a7613b7f7f9b2c2be90fd5c5efada
crgowen <private_email>
2023-09-09 09:23:53 +0100

    improved readability of testHelper code

--- test/beforeAllHelper.js
+++ test/beforeAllHelper.js
@@ -2,12 +2,21 @@ const server = require("../scripts/server.js");
 const process = require("process");
 
 module.exports = () => {
+    /*
+        When local or on-push triggered test, useGithubPagesSite will be false
+        but for the automatic daily test, it is true.
+
+        The automatic daily will test the site as hosted on Github Pages, rather than building and localhosting
+
+    */
     const useGithubPagesSite = process.env.USE_GH_PAGES_SITE === "true";
 
-    let app = null;
-    let url = "https://crgowen.github.io";
+    let app, url;
 
-    if (!useGithubPagesSite) {
+    if (useGithubPagesSite) {
+        app = null;
+        url = "https://crgowen.github.io";
+    } else {
         app = server("dist");
         const port = app.address().port;
         url = `http://localhost:${port}`;
